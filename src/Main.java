ac7d71512c29fadb27a0d48c4f9f3e60ae9db0e3
Toshiki Kamio <41144380+tcamio@users.noreply.github.com>
2021-05-07 02:03:21 -0600

    Completed algorithm. need to check by running an example.

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,57 @@ public class Main {
                 return false;
             }
             return true;
+        } else if (s.connective == "iff") {
+            Sentence left = s.children[0];
+            Sentence right = s.children[1];
+            if (PL_True(left, model) == PL_True(right, model)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (s.connective == "not") {
+            Sentence child = s.children[0];
+            return !PL_True(child, model);
+        } else {
+            System.out.println("The connective is wrong: " + s.connective);
+            return false;
+        }
+    }
+
+    public Boolean TT_Entails(Sentence KB, Sentence alpha) {
+        ArrayList<String> KBSymbols = getSymbols(KB);
+        ArrayList<String> alphaSymbols = getSymbols(alpha);
+
+        // Copy the knowledge base list
+        ArrayList<String> allSymbols = new ArrayList<>(KBSymbols);
+
+        // Add alpha symbol list
+        allSymbols.addAll(alphaSymbols);
+
+        //
+        HashMap<String, Boolean> model = new HashMap<>();
+
+        return TT_Check_All(KB, alpha, allSymbols, model);
+    }
+
+    public Boolean TT_Check_All(Sentence KB, Sentence alpha, ArrayList<String> symbols, HashMap<String, Boolean> model) {
+        if (symbols.isEmpty()) {
+            if (PL_True(KB, model)) {
+                return PL_True(alpha, model);
+            } else {
+                return true; // When KB is false, always return true
+            }
+        } else {
+            String P = symbols.get(0);
+            ArrayList<String> rest = new ArrayList<>(symbols);
+            rest.remove(0);
+
+            HashMap<String, Boolean> m1 = new HashMap<>(model);
+            HashMap<String, Boolean> m2 = new HashMap<>(model);
+            m1.put(P, true);
+            m2.put(P, false);
+
+            return TT_Check_All(KB, alpha, rest, m1) && TT_Check_All(KB, alpha, rest, m2);
         }
     }
 }
